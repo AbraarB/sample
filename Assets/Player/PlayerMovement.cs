ff7a39fe2e0ff30dd13f8ae7723d0b9d500d4c3d
AbraarB <abraarbimasakti@student.ub.ac.id>
2022-03-05 21:52:19 +0700

    Initial commit

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float playerWalkspeed = 5f;
+    [SerializeField] float jumpHeight = 3f;
+
+    Vector2 moveInput;
+    Rigidbody2D playerRigidbody;
+    CapsuleCollider2D playerBodycollider;
+    BoxCollider2D playerFeetcollider;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerBodycollider = GetComponent<CapsuleCollider2D>();
+        playerFeetcollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        Walk();
+        FlipSprite();
+    }
+
+    //input key jalan (A, D)
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    //input key loncat (space)
+    void OnJump(InputValue value)
+    {
+        if (!playerFeetcollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+        if (value.isPressed)
+        {
+            playerRigidbody.velocity += new Vector2(0f, jumpHeight);
+        }
+    }
+
+    //kecepatan jalan
+    void Walk()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * playerWalkspeed, playerRigidbody.velocity.y);
+        playerRigidbody.velocity = playerVelocity;
+    }
+
+    //player hadap kanan/kiri
+    void FlipSprite()
+    {
+        bool isPlayerMoving = Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (isPlayerMoving)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(playerRigidbody.velocity.x), 1f);
+        }
+    }
+}
