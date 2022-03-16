c06ab6d7d3ac6ecc0e1bdbae458a5f9dff09f914
hafidz0098 <hafidznak123@gmail.com>
2022-03-17 04:16:05 +0700

    add undead anim

--- Assets/Script/NcProjectileBehavior.cs
+++ Assets/Script/NcProjectileBehavior.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class NcProjectileBehavior : MonoBehaviour
 {
     [SerializeField]
-    float speed = 3f;
-    [SerializeField]
     int damage;
 
     float timeDestroy = 3f;
--- Assets/Script/UndeadPatrol.cs
+++ Assets/Script/UndeadPatrol.cs
@@ -8,28 +8,47 @@ public class UndeadPatrol : MonoBehaviour
     private Rigidbody2D rb;
     public bool balik;
     private SpriteRenderer sprite;
+    [SerializeField]
+    private int health;
+    private enum MovementState {undead_walk, undead_dead}
+    private Animator anim;
+    private float delay = 0.9f;
+    private bool hidup = true;
 
+
+    MovementState state;
     void Start()
     {
         balik = true;
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (balik)
+        if (hidup)
         {
-            rb.velocity = new Vector2(ms, rb.velocity.y);
-            sprite.flipX = true;
+            state = MovementState.undead_walk;
+            if (balik)
+            {
+                rb.velocity = new Vector2(ms, rb.velocity.y);
+                sprite.flipX = true;
 
+            }
+            else
+            {
+                rb.velocity = new Vector2(-ms, rb.velocity.y);
+                sprite.flipX = false;
+            };
         }
         else
         {
-            rb.velocity = new Vector2(-ms, rb.velocity.y);
-            sprite.flipX = false;
+            state = MovementState.undead_dead;
         }
+        anim.SetInteger("state", (int)state);
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -38,5 +57,22 @@ public class UndeadPatrol : MonoBehaviour
         {
             balik = !balik;
         }
+
+        if (collision.CompareTag("projectile"))
+        {
+            Destroy(collision.gameObject);
+            health -= 3;
+            if (health <= 0)
+            {
+                hidup = false;
+                ms = 0;
+                Invoke("undeadDestroy", delay);
+            }
+        }
+    }
+
+    void undeadDestroy()
+    {
+        Destroy(this.gameObject);
     }
 }
